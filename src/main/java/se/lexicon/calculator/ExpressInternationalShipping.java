7f28554d20f701dc0f5aa0d2a99dec59d56da385
Mohamad Nour Shich Khalil <mohmad200.mm16@gmail.com>
2026-01-08 11:13:50 +0100

    ExpressInternationalShipping.java is done

--- src/main/java/se/lexicon/calculator/ExpressInternationalShipping.java
+++ src/main/java/se/lexicon/calculator/ExpressInternationalShipping.java
@@ -4,14 +4,32 @@ import se.lexicon.model.Destination;
 import se.lexicon.model.ShippingRequest;
 import se.lexicon.model.Speed;
 import se.lexicon.service.ShippingCostCalculator;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import jakarta.annotation.PostConstruct;
 
+@Component
 public class ExpressInternationalShipping implements ShippingCostCalculator {
+
+
+    @Value("${shipping.international.express.base:25}")
+    private double baseCost;
+
+    @Value("${shipping.international.express.perkg:4.5}")
+    private double costPerKg;
+
 
     public boolean supports(ShippingRequest r) {
         return r.destination() == Destination.INTERNATIONAL && r.speed() == Speed.EXPRESS;
     }
 
+
     public double calculate(ShippingRequest r) {
-        return 25 + 4.5 * r.weightKg();
+        return baseCost + costPerKg * r.weightKg();
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Spring: ExpressInternationalShipping bean created");
     }
 }
