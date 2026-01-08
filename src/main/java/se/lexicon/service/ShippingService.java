6bfdaa1ee8982577cbe7b1724c567ca451da0566
Mohamad Nour Shich Khalil <mohmad200.mm16@gmail.com>
2026-01-08 11:15:17 +0100

    ShippingService.java is done

--- src/main/java/se/lexicon/service/ShippingService.java
+++ src/main/java/se/lexicon/service/ShippingService.java
@@ -1,13 +1,18 @@
 package se.lexicon.service;
 
 import se.lexicon.model.ShippingRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ShippingService {
     private final ShippingCalculatorFactory factory;
 
+    @Autowired
     public ShippingService(ShippingCalculatorFactory factory) {
         this.factory = factory;
     }
+
 
     public double quote(ShippingRequest req) {
         ShippingCostCalculator calc = factory.getCalculator(req);
