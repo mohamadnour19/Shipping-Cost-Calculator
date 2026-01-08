2e2db5192d1c3bba35fb3f306ed92528f94a3d7f
Mohamad Nour Shich Khalil <mohmad200.mm16@gmail.com>
2026-01-08 11:13:22 +0100

    StandardDomesticShipping.java is done

--- src/main/java/se/lexicon/calculator/StandardDomesticShipping.java
+++ src/main/java/se/lexicon/calculator/StandardDomesticShipping.java
@@ -4,13 +4,29 @@ import se.lexicon.model.Destination;
 import se.lexicon.model.ShippingRequest;
 import se.lexicon.model.Speed;
 import se.lexicon.service.ShippingCostCalculator;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import jakarta.annotation.PostConstruct;
 
+@Component
 public class StandardDomesticShipping implements ShippingCostCalculator {
+
+    @Value("${shipping.domestic.standard.base:5}")
+    private double baseCost;
+
+    @Value("${shipping.domestic.standard.perkg:1.2}")
+    private double costPerKg;
+
     public boolean supports(ShippingRequest r) {
         return r.destination() == Destination.DOMESTIC && r.speed() == Speed.STANDARD;
     }
 
     public double calculate(ShippingRequest r) {
-        return 5 + 1.2 * r.weightKg();
+        return baseCost + costPerKg * r.weightKg();
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Spring: StandardDomesticShipping bean created");
     }
 }
