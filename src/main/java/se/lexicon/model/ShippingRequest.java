df4c5b41b82c97adfd00d7d833b9d3dc7d96115e
Mohamad Nour Shich Khalil <mohmad200.mm16@gmail.com>
2026-01-08 11:11:46 +0100

    ShippingRequest.java is done

--- src/main/java/se/lexicon/model/ShippingRequest.java
+++ src/main/java/se/lexicon/model/ShippingRequest.java
@@ -1,7 +1,48 @@
 package se.lexicon.model;
 
+public class ShippingRequest {
+    private Destination destination;
+    private Speed speed;
+    private double weight;
 
-public record ShippingRequest(Destination destination, Speed speed, double weightKg) {
+
+    public ShippingRequest(Destination destination, Speed speed, double weight) {
+        this.destination = destination;
+        this.speed = speed;
+        this.weight = weight;
+    }
 
 
+    public Destination destination() {
+        return destination;
+    }
+
+    public Speed speed() {
+        return speed;
+    }
+
+    public double weightKg() {
+        return weight;
+    }
+
+
+    public Destination getDestination() {
+        return destination;
+    }
+
+    public Speed getSpeed() {
+        return speed;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    @Override
+    public String toString() {
+        return "ShippingRequest{" +
+                "destination=" + destination +
+                ", speed=" + speed +
+                ", weight=" + weight + '}';
+    }
 }
